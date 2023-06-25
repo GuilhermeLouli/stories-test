362ee0755de1ff01c72e3e5b12fee2f93fa721a2
GuilhermeLouli <guilhermeloulisoares@gmail.com>
2023-06-25 17:51:23 -0300

    feat(story): add gradient

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -1,6 +1,4 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { StyleSheet, View } from 'react-native';
-import Story from '../Story';
 
 export default function Stories() {
 
@@ -9,18 +7,14 @@ export default function Stories() {
       colors={['#97A3E2', 'white']}
       start={{ x: -2, y: .3 }}
       end={{ x: .6, y: 0 }}
-      style={styles.curve}
+      style={{
+        position: "absolute",
+        width: 300,
+        height: 300,
+        top: -80,
+        borderRadius: 300,
+        transform: [{ scaleX: 2 }],
+      }}
     />
   )
 }
-
-const styles = StyleSheet.create({
-  curve: {
-    position: "absolute",
-    width: 300,
-    height: 300,
-    top: -80,
-    borderRadius: 300,
-    transform: [{ scaleX: 2 }],
-  }
-});
--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -1,13 +1,19 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Story({ name }) {
   return (
     <>
-      <View style={styles.circle}>
+      <LinearGradient
+        colors={['#97A3E2', '#3E59E8']}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 1 }}
+        style={styles.circle}
+      >
         <View style={styles.circle2}>
           <View style={styles.photo} />
         </View>
-      </View>
+      </LinearGradient>
 
       <Text style={styles.text}>@derec_collins</Text>
     </>
@@ -18,16 +24,15 @@ const styles = StyleSheet.create({
   circle: {
     width: 65,
     height: 65,
-    backgroundColor: "#5F85DC",
     borderRadius: 65,
     justifyContent: "center",
     alignItems: "center"
   },
   circle2: {
-    width: 60,
-    height: 60,
+    width: 59,
+    height: 59,
+    borderRadius: 59,
     backgroundColor: "white",
-    borderRadius: 60,
     justifyContent: "center",
     alignItems: "center"
   },
