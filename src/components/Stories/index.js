3ec8d80745c7e8fb34bd1a1551ea70d395853163
GuilhermeLouli <guilhermeloulisoares@gmail.com>
2023-06-26 11:46:06 -0300

    feat(stories): improve stories logic

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -1,54 +1,47 @@
-import React, { useMemo, useEffect, useState } from 'react';
-import { View, StyleSheet, Dimensions, PanResponder, Text } from 'react-native';
+import React, { useMemo, useState } from 'react';
+import { View, StyleSheet, PanResponder } from 'react-native';
 import Story from '../Story';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Stories() {
-  const storySize = 75;
+
+  const storyStyleSize = 75;
   const wheelRadius = 540;
   const numStories = 40;
-  const offset = 12;
+  const radiusDefaultOffset = 12;
 
-  const [storiesPositions, setStoriesPositions] = useState(() =>
-    Array.from({ length: numStories }).map((_, index) => getStoryPosition(index))
-  );
-
-  const panResponder = useMemo(
-    () =>
-      PanResponder.create({
-        onStartShouldSetPanResponder: () => true,
-        onPanResponderMove: (event, gestureState) => {
-          setStoriesPositions((prevPositions) => {
-            const radiusOffset = Math.sqrt(gestureState.dx ** 2 + gestureState.dy ** 2);
-
-            const updatedPositions = prevPositions.map((position, index) => {
-              const angle = ((numStories - index + offset) * 2 * Math.PI) / numStories;
-              const x = wheelRadius * Math.cos(angle + radiusOffset / wheelRadius) - storySize / 2;
-              const y = wheelRadius * Math.sin(angle + radiusOffset / wheelRadius) - storySize / 2;
-              return { x, y };
-            });
-
-            return updatedPositions;
-          });
-        },
-      }),
-    []
-  );
-
-  useEffect(() => {
-    return () => {
-      setStoriesPositions((prevPositions) =>
-        prevPositions.map((_, index) => getStoryPosition(index))
-      );
-    };
-  }, []);
-
-  function getStoryPosition(index) {
-    const angle = ((numStories - index + offset) * 2 * Math.PI) / numStories;
-    const x = wheelRadius * Math.cos(angle) - storySize / 2;
-    const y = wheelRadius * Math.sin(angle) - storySize / 2;
+  const getStoryPosition = index => {
+    const angle = ((numStories - index + radiusDefaultOffset) * 2 * Math.PI) / numStories;
+    const x = wheelRadius * Math.cos(angle) - storyStyleSize / 2;
+    const y = wheelRadius * Math.sin(angle) - storyStyleSize / 2;
     return { x, y };
   }
+
+  const [storiesPositions, setStoriesPositions] = useState(Array.from({ length: numStories }).map((_, i) => getStoryPosition(i)))
+  const [radiusOffset] = useState({ value: 0 })
+
+  const panResponder = useMemo(() =>
+    PanResponder.create({
+
+      onStartShouldSetPanResponder: () => true,
+
+      onPanResponderMove: (_, gestureState) => {
+
+        const newRadiusOffset = radiusOffset.value + (-Math.sign(gestureState.dx) * Math.sqrt(gestureState.dx ** 2))
+
+        setStoriesPositions(s => s.map((_, index) => {
+          const angle = ((numStories - index + radiusDefaultOffset) * 2 * Math.PI) / numStories;
+          const x = wheelRadius * Math.cos(angle + newRadiusOffset / wheelRadius) - storyStyleSize / 2;
+          const y = wheelRadius * Math.sin(angle + newRadiusOffset / wheelRadius) - storyStyleSize / 2;
+          return { x, y };
+        })
+        )
+      },
+
+      onPanResponderRelease: (_, gestureState) => radiusOffset.value += (-Math.sign(gestureState.dx) * Math.sqrt(gestureState.dx ** 2))
+    }),
+    []
+  );
 
   return (
     <>
@@ -60,12 +53,15 @@ export default function Stories() {
       />
 
       <View style={styles.centerButton} />
+
       <View style={styles.container} {...panResponder.panHandlers}>
-        {storiesPositions.map(({ x, y }, index) => (
-          <View key={index} style={[styles.story, { left: x, top: y }]}>
+
+        {storiesPositions.map((item, index) => (
+          <View key={index} style={[styles.story, { left: item.x, top: item.y }]}>
             <Story name={index} />
           </View>
         ))}
+
       </View>
     </>
   );
